4e47dd9e28dd54a216be936fccf1347054d93aab
Sergey Samoilenko <sergey.samoilenko@dev-pro.net>
2019-01-14 08:49:58 +0200

    init

---
+++ app/src/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink, BrowserRouter as Router, Route } from 'react-router-dom';
+
+function HomePage() {
+    return (
+        <div>
+            <h2>HomePage</h2>
+        </div>
+    );
+}
+function CatsPage() {
+    return (
+        <div>
+            <h2>CatsPage</h2>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <Router>
+            <nav>
+                <NavLink to="/" activeClassName="active">Home</NavLink>
+                {" | "}
+                <NavLink to="/cats" activeClassName="active">Cats</NavLink>
+
+                {/*<Route path="/" exact component={HomePage}/>*/}
+                {/*<Route path="/cats" component={CatsPage} />*/}
+            </nav>
+        </Router>
+    );
+};
+
+export default Header;
---
+++ app/src/routes.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+// import App from './components/App';
+// import HomePage from './components/home/HomePage';
+// import CatsPage from './components/cats/CatsPage';
+// import CatPage from './components/cats/CatPage';
+
+function HomePage() {
+    return (
+        <div>
+            <h2>HomePage</h2>
+        </div>
+    );
+}
+function CatsPage() {
+    return (
+        <div>
+            <h2>CatsPage</h2>
+        </div>
+    );
+}
+
+export default () => (
+<Router>
+    <Switch>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/cats">Cats</Link>
+                </li>
+            </ul>
+
+            <hr />
+
+            <Route path="/" exact component={HomePage}/>
+            <Route path="/cats" component={CatsPage} />
+        </div>
+    </Switch>
+</Router>
+);
